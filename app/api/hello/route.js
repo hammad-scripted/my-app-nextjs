43dd9ddf6e779706c659bdc3b02975255fc76820
Mohammad Hammad <hammad.scripted@gmail.com>
2026-06-03 22:54:26 +0530

    Refactor GET API route to include search parameters and remove unused route file

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,7 +1,12 @@
 import { NextResponse } from 'next/server';
 
 export async function GET(request) {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const url = new URL(request.url);
+  const { searchParams } = url;
+  console.log(searchParams);
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?${searchParams.toString()}`,
+  );
   const data = await response.json();
   return NextResponse.json({ data }, { status: 200 });
 }
--- app/api/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET(request) {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const data = await response.json();
-  console.log(data);
-
-  return NextResponse.json({ data }, { status: 200 });
-}
